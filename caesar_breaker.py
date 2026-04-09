edfe435f27aafca2a79904fd07976cca5c77ef44
raccis <lorenzo.raccis@edu-its.it>
2026-04-09 19:27:18 +0200

    secondo commit, il programma funziona ed evidenzia in italiano. Devo ancora fare i test ma per ora ci siamo EH!!

--- caesar_breaker.py
+++ caesar_breaker.py
@@ -1,5 +1,5 @@
-
-
+ROSSO   = "\033[31m" #per colorare le scritte
+RESET   = "\033[0m"
 
 def brute_force(testo:str,key:int,numeri:list) -> list[tuple[int, str]] :
 
@@ -17,19 +17,25 @@ def brute_force(testo:str,key:int,numeri:list) -> list[tuple[int, str]] :
                 numeri[idx] = (numeri[idx] - key) % 26
                 numeri[idx] += 97
         numeri[idx] = chr(numeri[idx])
-        i=testo[idx]
     return numeri
 
 
 
 
 def main():
+    parole = ["di", "che", "e", "il", "la"] #parole comuni nell'italiano
     print("!!Brute forcing del cifrario di cesare!!\n")
     testo = input("Inserire il testo da decifrare : ")
     n=0
     while n <26 :
         numeri = []
-        print("".join(brute_force(testo,n,numeri)))
+        print (f"chiave {n}") #cosi capiamo quale chiave è stata usata per ciascuna decifratura
+        decifrato ="".join(brute_force(testo,n,numeri)) #mettiamo la decifratura in una variabile
+        individua_parole = decifrato.split() #separiamo i caratteri facendoli diventare parole
+        if any(parola in individua_parole for parola in parole): #controlla ogni parola nella frase e vede se c'è almeno un elemento della lista nella frase
+                print(f"{ROSSO}{decifrato}{RESET}") #printa in rosso
+        else:
+            print(f"{decifrato}")
         print("---------------------------------------------")
         n+=1
     print("\nFINITO!!")

f125acd4d5515f2c6b770c36bb40d4199f6f2b0a
raccis <lorenzo.raccis@edu-its.it>
2026-04-10 15:30:23 +0200

    Terzo commit, ho adattato il programma alle richieste del test base

--- caesar_breaker.py
+++ caesar_breaker.py
@@ -1,23 +1,27 @@
 ROSSO   = "\033[31m" #per colorare le scritte
 RESET   = "\033[0m"
 
-def brute_force(testo:str,key:int,numeri:list) -> list[tuple[int, str]] :
+def brute_force(testo:str,key:int =0,numeri:list = None) -> list[tuple[int, str]] :
+    ris = [] #sta lista contiene le decifrazioni
+    for key in range(26) :
+        numeri = []
+        for idx in range(len(testo)):
+            i=testo[idx]
+            numeri.append(testo[idx])
+            numeri[idx] = ord(numeri[idx])
+            if i.isalpha():
+                if i.isupper():
+                    numeri[idx] -= 65
+                    numeri[idx] = (numeri[idx] + key) % 26
+                    numeri[idx] += 65
+                elif i.islower():
+                    numeri[idx] -= 97
+                    numeri[idx] = (numeri[idx] + key) % 26
+                    numeri[idx] += 97
+            numeri[idx] = chr(numeri[idx])
+        ris.append((key, "".join(numeri))) #stampa chiave + la tupla con le decifrature oh yes
+    return ris #ritorna l'insieme di tutte le cifrature fatte
 
-    for idx in range(len(testo)):
-        i=testo[idx]
-        numeri.append(testo[idx])
-        numeri[idx] = ord(numeri[idx])
-        if i.isalpha():
-            if i.isupper():
-                numeri[idx] -= 65
-                numeri[idx] = (numeri[idx] - key) % 26
-                numeri[idx] += 65
-            elif i.islower():
-                numeri[idx] -= 97
-                numeri[idx] = (numeri[idx] - key) % 26
-                numeri[idx] += 97
-        numeri[idx] = chr(numeri[idx])
-    return numeri
 
 
 
@@ -26,18 +30,7 @@ def main():
     parole = ["di", "che", "e", "il", "la"] #parole comuni nell'italiano
     print("!!Brute forcing del cifrario di cesare!!\n")
     testo = input("Inserire il testo da decifrare : ")
-    n=0
-    while n <26 :
-        numeri = []
-        print (f"chiave {n}") #cosi capiamo quale chiave è stata usata per ciascuna decifratura
-        decifrato ="".join(brute_force(testo,n,numeri)) #mettiamo la decifratura in una variabile
-        individua_parole = decifrato.split() #separiamo i caratteri facendoli diventare parole
-        if any(parola in individua_parole for parola in parole): #controlla ogni parola nella frase e vede se c'è almeno un elemento della lista nella frase
-                print(f"{ROSSO}{decifrato}{RESET}") #printa in rosso
-        else:
-            print(f"{decifrato}")
-        print("---------------------------------------------")
-        n+=1
+    print (brute_force(testo,key=0,numeri=[]))
     print("\nFINITO!!")
 
 
